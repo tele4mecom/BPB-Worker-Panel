8b1c8ec3ba14c73effa66bd88ecae9ce3a9e9694
tele4mecom <55290637+tele4mecom@users.noreply.github.com>
2024-05-24 16:19:23 +0330

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -792,7 +792,7 @@ const getNormalConfigs = async (env, hostName, client) => {
         let remark = `💦 BPB - ${addr}`;
         remark = remark.length <= 30 ? remark : `${remark.slice(0,29)}...`;
 
-        vlessWsTls += 'vless' + `://${userID}@${addr}:80?encryption=none&security=tls&type=ws&host=${
+        vlessWsTls += 'vless' + `://${userID}@${addr}:443?encryption=none&security=tls&type=ws&host=${
             randomUpperCase(hostName)
         }&sni=${
             randomUpperCase(hostName)
